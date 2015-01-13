dc379da72883bb1b3c7a53597e8a9ef374175232
Neza <neza.dimec@gmail.com>
2015-01-13 22:21:09 +0100

    Pozabila na en zamik.

--- gui_demo.py
+++ gui_demo.py
@@ -14,51 +14,51 @@ class Imena:
             podatki = re.findall(vzorec, stran)
             return podatki
 
-def prenos2(name):
-    name = name.capitalize()
-    povezava = 'http://sl.wikipedia.org/wiki/{0}'.format(name)
-    with urllib.request.urlopen(povezava) as f:
-        stran = str(f.read())
-##    name = name.capitalize()
-##    ime = quote(name, encoding="utf-8")
-##    povezava = 'http://sl.wikipedia.org/wiki/{0}'.format(ime)
-##    with urllib.request.urlopen(povezava) as f:
-##        stran = f.read().decode("utf-8")
+    def prenos2(name):
+        name = name.capitalize()
+        povezava = 'http://sl.wikipedia.org/wiki/{0}'.format(name)
+        with urllib.request.urlopen(povezava) as f:
+            stran = str(f.read())
+    ##    name = name.capitalize()
+    ##    ime = quote(name, encoding="utf-8")
+    ##    povezava = 'http://sl.wikipedia.org/wiki/{0}'.format(ime)
+    ##    with urllib.request.urlopen(povezava) as f:
+    ##        stran = f.read().decode("utf-8")
 
-        sl={'pomen': ["Ni podatka"],
-            'izvor': ["Ni podatka"],
-            'izvorna oblika': ["Ni podatka"],
-            'god': ["Ni podatka"]} #to je slovar, ki bo vseboval vse podatke
-        
-        vzorec_pomen = re.compile(r'<th>Pomen</th>\\n<td><i>(.*)</i></td>')
-        pomen = re.findall(vzorec_pomen, stran) #seznam z 1 elementom (niz)
-        print(pomen)
-        if pomen != []:
-            sl["pomen"] = pomen
-        
-
-        vzorec_izvor = re.compile(r'<th>Izvor</th>\\n<td>(.[^<]*\w+)</td>')
-        izvor = re.findall(vzorec_izvor, stran)
-        print(izvor)
-        if izvor != []:
-            sl["izvor"] = izvor
+            sl={'pomen': ["Ni podatka"],
+                'izvor': ["Ni podatka"],
+                'izvorna oblika': ["Ni podatka"],
+                'god': ["Ni podatka"]} #to je slovar, ki bo vseboval vse podatke
+            
+            vzorec_pomen = re.compile(r'<th>Pomen</th>\\n<td><i>(.*)</i></td>')
+            pomen = re.findall(vzorec_pomen, stran) #seznam z 1 elementom (niz)
+            print(pomen)
+            if pomen != []:
+                sl["pomen"] = pomen
             
 
-        vzorec_izvorna_oblika = re.compile(r'<th>Izvorna oblika</th>\\n<td>(.[^<]*\w+)</td>')
-        #problem, ce je poleg se kaksen link v oklepaju itd.
-        izvorna_oblika = re.findall(vzorec_izvorna_oblika, stran)
-        print(izvorna_oblika)
-        if izvorna_oblika != []:
-            sl["izvorna oblika"] = izvorna_oblika
-            
+            vzorec_izvor = re.compile(r'<th>Izvor</th>\\n<td>(.[^<]*\w+)</td>')
+            izvor = re.findall(vzorec_izvor, stran)
+            print(izvor)
+            if izvor != []:
+                sl["izvor"] = izvor
+                
 
-        vzorec_god = re.compile(r'<th>God</th>\\n<td>(.[^<]*\w+)</td>')
-        god = re.findall(vzorec_god, stran)
-        print(god)
-        if god != []:
-            sl["god"] = god
+            vzorec_izvorna_oblika = re.compile(r'<th>Izvorna oblika</th>\\n<td>(.[^<]*\w+)</td>')
+            #problem, ce je poleg se kaksen link v oklepaju itd.
+            izvorna_oblika = re.findall(vzorec_izvorna_oblika, stran)
+            print(izvorna_oblika)
+            if izvorna_oblika != []:
+                sl["izvorna oblika"] = izvorna_oblika
+                
 
-        return sl
+            vzorec_god = re.compile(r'<th>God</th>\\n<td>(.[^<]*\w+)</td>')
+            god = re.findall(vzorec_god, stran)
+            print(god)
+            if god != []:
+                sl["god"] = god
+
+            return sl
     
     def prikazi(self, *argumenti):
         print('Kliknili so me!')
